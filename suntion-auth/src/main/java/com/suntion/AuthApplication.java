c26e4e86ab358213a4de0f61e5d32f39c94dd87a
大迷髦 <suntion@yeah.net>
2019-03-08 17:39:22 +0800

    修改日志地址

--- suntion-auth/src/main/java/com/suntion/AuthApplication.java
+++ suntion-auth/src/main/java/com/suntion/AuthApplication.java
@@ -1,15 +1,27 @@
 package com.suntion;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.core.io.support.ResourcePropertySource;
+
+import java.io.IOException;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class AuthApplication {
 
-    public static void main(String[] args) {
+    private static final Logger logger = LoggerFactory.getLogger(AuthApplication.class);
+
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(AuthApplication.class, args);
+        logger.info("info");
+        logger.error("error");
+        logger.debug("debug");
+        System.out.println("启动。。。");
     }
 
 }
