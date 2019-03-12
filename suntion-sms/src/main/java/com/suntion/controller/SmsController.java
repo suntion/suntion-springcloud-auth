7931035080052af852eff748766f9ab414165ce5
大迷髦 <suntion@yeah.net>
2019-03-12 17:20:55 +0800

    修改fegin和hystrix超时时间

--- suntion-sms/src/main/java/com/suntion/controller/SmsController.java
+++ suntion-sms/src/main/java/com/suntion/controller/SmsController.java
@@ -1,5 +1,6 @@
 package com.suntion.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,9 +10,26 @@ public class SmsController {
     @Value("${server.port}")
     private Integer port;
 
-    @RequestMapping("/sms/{phone}/{content}")
+    @HystrixCommand(fallbackMethod = "error")
+    @RequestMapping("/sms/{content}/{phone}")
     public String sms(@PathVariable String phone, @PathVariable String content) {
+        if(phone.startsWith("e")) {
+            throw  new RuntimeException("sms错误");
+        }
+
+        if (content.equals("超时")) {
+            try {
+                Thread.sleep(4500);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
         return phone+"-" + content + "-----"+port;
+    }
+
+    public String error(@PathVariable String phone, @PathVariable String content) {
+        //发起某个网络请求（可能失败）
+        return "sms中发生熔断";
     }
 
     @GetMapping("/sms/test")
