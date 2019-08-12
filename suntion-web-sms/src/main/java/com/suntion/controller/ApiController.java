a091f8ce7e04d16b6d6e9cee6dbb423a4c510f51
scott <pai=3.1415926>
2019-08-12 11:50:44 +0800

    sms发生短信

--- suntion-web-sms/src/main/java/com/suntion/controller/ApiController.java
+++ suntion-web-sms/src/main/java/com/suntion/controller/ApiController.java
@@ -1,12 +1,18 @@
 package com.suntion.controller;
 
+import com.aliyuncs.CommonRequest;
+import com.aliyuncs.CommonResponse;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.profile.DefaultProfile;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.suntion.common.lang.ResponseEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.concurrent.TimeoutException;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author suntion
@@ -19,12 +25,23 @@ public class ApiController {
     @HystrixCommand(fallbackMethod = "error")
     @RequestMapping("/{phone}/{content}")
     public ResponseEntity sms(@PathVariable String phone, @PathVariable String content) throws Exception{
-        if (90< Integer.valueOf(content) && Integer.valueOf(content) < 95) {
-            return ResponseEntity.failed().setResult("短信发送失败");
-        }
-        if (content.equals("1")) {
-            throw new TimeoutException("连接超时");
-        }
+        //DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI109kOmJOeMlj", "5u7Ef7uX9emLqkNYjluUr1AbrUbT8e");
+        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "123123", "123123");
+        IAcsClient client = new DefaultAcsClient(profile);
+
+        CommonRequest request = new CommonRequest();
+        request.setMethod(MethodType.POST);
+        request.setDomain("dysmsapi.aliyuncs.com");
+        request.setVersion("2017-05-25");
+        request.setAction("SendSms");
+        request.putQueryParameter("RegionId", "cn-hangzhou");
+        request.putQueryParameter("PhoneNumbers", phone);
+        request.putQueryParameter("SignName", "大迷髦");
+        request.putQueryParameter("TemplateCode", "SMS_165416562");
+        request.putQueryParameter("TemplateParam", "{code:"+content+"}");
+        CommonResponse response = client.getCommonResponse(request);
+        logger.info("Send SMS {}, response:{}",phone, response.getData());
+
 
         logger.info("短信发送成功");
         return ResponseEntity.success().setResult("短信发送成功");
